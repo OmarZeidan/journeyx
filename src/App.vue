0f068bcf547c3518d9c6578a21712339bd05333b
Omar Zeidan <iam.zdeveloper@gmail.com>
2025-02-13 21:16:53 +0100

    feat: first init vue w/typescript and some clean up

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import IconLogo from './components/icons/IconLogo.vue'
+</script>
+
+<template>
+  <header>
+    <div class="container mx-auto flex p-5 items-center">
+      <a href="/" class="flex items-center text-indigo-500">
+        <IconLogo />
+        <span class="ml-2 text-xl text-gray-900">Journey X</span>
+      </a>
+    </div>
+  </header>
+
+  <footer class="bg-white shadow-sm dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        Made with 💜 by Omar Zeidan.
+      </span>
+    </div>
+  </footer>
+</template>
